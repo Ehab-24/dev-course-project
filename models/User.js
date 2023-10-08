15d6889151b23dc4e95384962c748bc33da8d19e
Ehab-24 <ehabs1775@gmail.com>
2023-10-08 09:26:13 +0500

    initial commit

---
+++ models/User.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User;
